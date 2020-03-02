cd9c1440defc2749fd829f44446d571cc81a0874
Andrew <cc6.magister@gmail.com>
2020-03-02 18:10:38 +0200

    add player logic

--- src/components/player/index.jsx
+++ src/components/player/index.jsx
@@ -7,6 +7,7 @@ import Pause from "./pause";
 import Play from "./play";
 import UnMute from "./unmute";
 import Mute from "./mute";
+import Arrow from "./arrow";
 
 let player = React.createRef(),
 	_raf;
@@ -15,6 +16,11 @@ const songs = [
 		name: 'Фрагмент 31,<br> Сапфо',
 		src: testSong,
 		id: 0
+	},
+	{
+		name: 'Фрагмент 31,<br> Сапфо 2',
+		src: testSong,
+		id: 1
 	}
 ];
 
@@ -104,9 +110,24 @@ const Player = () => {
 		})
 	};
 
+	const handleNext = (e,side = false) => {
+		if (side) {
+			setSong(prevState => ({
+				...prevState,
+				activeSong: prevState.songs[(prevState.activeSong.id - 1)]
+			}))
+
+		} else {
+			setSong(prevState => ({
+				...prevState,
+				activeSong: prevState.songs[prevState.activeSong.id + 1]
+			}))
+		}
+	};
+
 
 	return (
-		<>
+		<div className='player'>
 			<ReactHowler
 				src={[songController.activeSong.src]}
 				playing={playerValues.playing}
@@ -118,36 +139,48 @@ const Player = () => {
 				volume={playerValues.volume}
 				ref={player}
 			/>
-			<div className="player-wrapper">
-				<div className='control-wrapper'>
-					<button className='player-btn' onClick={handleToggle}>
-						{(playerValues.playing) ? <Pause/> : <Play/>}
-					</button>
-					<span className='player-status'>
-                    {(playerValues.seek !== undefined) ? updateTime(playerValues.seek) : '0:00'}
-						{' / '}
-						{(playerValues.duration) ? updateTime(playerValues.duration) : 'Loading...'}
-                    </span>
-				</div>
-				<span dangerouslySetInnerHTML={{__html: songController.activeSong.name}}/>
-				<div className='control-wrapper'>
-					<input
-						className='player-input'
-						type='range'
-						min='0'
-						max='1'
-						step='.05'
-						value={playerValues.volume}
-						style={{verticalAlign: 'bottom'}}
-						onChange={volumeHandler}
-					/>
-					<button className='player-btn' onClick={handleMute}>
-						{(playerValues.mute) ? <UnMute/> : <Mute/>}
-					</button>
+			<div className="container-fluid player-wrapper">
+				<div className="row">
 
+					<div className='col col-md-3 control-wrapper'>
+						<button disabled={songController.activeSong.id === 0} onClick={() => handleNext(e=null,'prev')}
+						        className="player-btn btn-arrow btn-m">
+							<Arrow/>
+						</button>
+						<button className='player-btn btn-m' onClick={handleToggle}>
+							{(playerValues.playing) ? <Pause/> : <Play/>}
+						</button>
+						<button disabled={songController.activeSong.id === songController.songs.length-1}
+						        onClick={handleNext} className="player-btn flip-horizontally btn-m">
+							<Arrow/>
+						</button>
+						<span className='player-status d-none d-sm-inline'>
+                            {(playerValues.seek !== undefined) ? updateTime(playerValues.seek) : '0:00'}
+							{' / '}
+							{(playerValues.duration) ? updateTime(playerValues.duration) : 'Loading...'}
+                        </span>
+					</div>
+					<span className='col' dangerouslySetInnerHTML={{__html: songController.activeSong.name}}/>
+					<div className='col col-sm-3 control-wrapper d-flex justify-content-end'>
+						<input
+							className='player-input d-none d-sm-inline'
+							type='range'
+							min='0'
+							max='1'
+							step='.05'
+							value={playerValues.volume}
+							style={{verticalAlign: 'bottom'}}
+							onChange={volumeHandler}
+						/>
+						<button className='player-btn' onClick={handleMute}>
+							{(playerValues.mute) ? <UnMute/> : <Mute/>}
+						</button>
+
+					</div>
 				</div>
+
 			</div>
-		</>
+		</div>
 	)
 };
 
